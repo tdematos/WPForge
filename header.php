1815822bd7dff39f935a3dcfb64c5fa1eb131ac0
Thiago de Matos <thiago.dematos.1@gmail.com>
2025-02-25 08:46:26 -0500

    add search button to nav and fix layout spacing

--- header.php
+++ header.php
@@ -2,19 +2,22 @@
 <html lang="en">
   <head>
   <?php wp_head(); ?>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   </head>
   <body>
-    <nav>
-      <div class="site-logo">WPForge</div>
+    <nav class="navigation-bar">
+      <div class="site-logo">WPFORGE</div>
       <div class="navigation">
         <ul class="nav-list">
-          <li class="nav-item">Home</li>
-          <li class="nav-item">Portfolio</li>
-          <li class="nav-item">Blog</li>
-          <li class="nav-item">Pricing</li>
-          <li class="nav-item">Contact</li>
+          <li class="nav-item"><a href="#">HHOME</a></li>
+          <li class="nav-item"><a href="#">PORTFOLIO</a></li>
+          <li class="nav-item"><a href="#">BLOG</a></li>
+          <li class="nav-item"><a href="#">PRICING</a></li>
+          <li class="nav-item"><a href="#">CONTACT</a></li>
         </ul>
       </div>
-      <div class="Logo"></div>
+      <div class="search">
+        <button class="search-btn"><i class="fa-solid fa-magnifying-glass"></i>SEARCH</button>
+      </div>
     </nav>
  
