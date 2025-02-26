fba8cb8ba40291ae4dbc76358bd94cc7f8125206
Thiago de Matos <thiago.dematos.1@gmail.com>
2025-02-26 08:42:37 -0500

    styled nav section and start working on banner and add style

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
       <div class="site-logo">WPFORGE</div>
       <div class="navigation">
         <ul class="nav-list">
-          <li class="nav-item"><a href="#">HHOME</a></li>
+          <li class="nav-item"><a href="#">HOME</a></li>
           <li class="nav-item"><a href="#">PORTFOLIO</a></li>
           <li class="nav-item"><a href="#">BLOG</a></li>
           <li class="nav-item"><a href="#">PRICING</a></li>
@@ -19,5 +19,21 @@
       <div class="search">
         <button class="search-btn"><i class="fa-solid fa-magnifying-glass"></i>SEARCH</button>
       </div>
+      <div class="connect-container">
+        <button class="connect-button">C</button>
+      </div>
     </nav>
+    <section class="hp-banner">
+        <div class="title-container">
+            <h2 class="banner-title-1">Wordpress Development</h2>
+            <h2 class="banner-title-2">Agency</h2>
+            <h3 class="banner-subtitle">Take your website to new heights by hiring WPForge for your next project.</h3>
+        </div>
+        <div class="banner-btn-container">
+            <button class="banner-cta-btn">CONNECT</button>
+        </div>
+        <div class="Agency Slogan">
+            <p>GLOBAL WP AGENCY </p>
+        </div>
+    </section>
  
