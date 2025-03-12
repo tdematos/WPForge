bd643df26852488105e60aba9e80cc7eb5e004ea
Thiago de Matos <thiago.dematos.1@gmail.com>
2025-03-12 08:35:50 -0400

    create and style footer

--- footer.php
+++ footer.php
@@ -6,26 +6,35 @@
         </div>
         <div class="footer-box-2">
             <p class="footer-description">We are an ecom agency that will help skyrocket your website needs</p>
-            <button class="footer-cta">LET'S TALK WITH US</button>
+            <button class="footer-cta-btn">LET'S TALK WITH US</button>
         </div>
     </div>
-    <div class="footer-section-1">
+    <div class="footer-section-2">
         <footer class="banner-1">
             <ul class="footer-nav-1">
-                <li class="footer-nav-items">ABOUT US</li>
-                <li class="footer-nav-items">CAREERS</li>
-                <li class="footer-nav-items">SERVICES</li>
-                <li class="footer-nav-items">CONTACT</li>
+                <li class="footer-nav-items"><a href="">ABOUT US</a></li>
+                <li class="footer-nav-items"><a href="">CAREERS</a></li>
+                <li class="footer-nav-items"><a href="">SERVICES</a></li>
+                <li class="footer-nav-items"><a href="">CONTACT</a></li>
             </ul>
         </footer>
         <footer class="banner-2">
         <ul class="footer-nav-2">
-                <li class="footer-nav-items">PRIVACY POLICY</li>
-                <li class="footer-nav-items">TERMS & CONDITIONS</li>
+                <li class="footer-nav-items"><a href="">PRIVACY POLICY</a></li>
+                <li class="footer-nav-items"><a href="">TERMS & CONDITIONS</a></li>
             </ul>
         </footer>
     </div>
-    <div class="footer-section-1"></div>
+    <div class="footer-section-3">
+        <div class="footer-icon-box">
+        <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-x-twitter"></i>
+        </div>
+        <div class="footer-cr-box">
+            <p class="copyright-footer">Copyright &copy; <?php echo date('Y'); ?></p>
+        </div>
+    </div>
 </section>
     
 </body>
