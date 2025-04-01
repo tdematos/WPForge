d7f0761838e165d8aa759499c2362c4eb6bf5e08
Thiago de Matos <thiago.dematos.1@gmail.com>
2025-04-01 15:56:00 -0400

    add video to hero section background, and fix section width

--- front-page.php
+++ front-page.php
@@ -51,17 +51,20 @@
     </div>
 </dialog>
     <section class="hp-banner">
-        <div class="title-container">
-            <h2 class="banner-title-1">Wordpress Development</h2>
-            <h2 class="banner-title-2">Agency</h2>
-            <h3 class="banner-subtitle">Take your website to new heights by hiring WPForge for your next project.</h3>
-        </div>
-        <div class="banner-btn-container">
-            <button class="banner-cta-btn">CONNECT</button>
-        </div>
-        <div class="agency-slogan">
-        <i class="fa-solid fa-globe globe-icon"></i>
-            <p>GLOBAL WP AGENCY </p>
+        <video autoplay muted loop src="<?php echo get_template_directory_uri(); ?>/assets/bannerVideo.webm" class="banner-video"></video>
+         <div class="banner-overlay">
+         <div class="title-container">
+                <h2 class="banner-title-1">Wordpress Development</h2>
+                <h2 class="banner-title-2">Agency</h2>
+                <h3 class="banner-subtitle">Take your website to new heights by hiring WPForge for your next project.</h3>
+          </div>
+         <div class="banner-btn-container">
+                <button class="banner-cta-btn">CONNECT</button>
+         </div>
+         <div class="agency-slogan">
+         <i class="fa-solid fa-globe globe-icon"></i>
+                <p>GLOBAL WP AGENCY </p>
+         </div>
         </div>
     </section>
     <section class="page-white-space"></section>
